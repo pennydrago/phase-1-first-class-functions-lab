feb176713ac0d1a1ab1cdd6056f0f489da9b24b5
Penelope Drago <prd@drago.com.au>
2023-03-18 15:06:52 +1100

    add variables and functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(driverNames) {
+  return driverNames.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(driverNames) {
+  return driverNames.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number) {
+  return function(fare) {
+    return fare * number
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driverNames, callback) {
+  return callback(driverNames);
+}
